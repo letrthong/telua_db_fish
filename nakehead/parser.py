3ef4f09ea78c65fa83830db6ab33d40c95b8fa69
Thong LT <letrthong@gmail.com>
2024-07-07 17:46:58 +0700

    Update parser.py

--- nakehead/parser.py
+++ nakehead/parser.py
@@ -2,11 +2,16 @@ import json
 import logging
 
  
+
 file = open("nakehead_feeder.json")
 data_object = json.load(file)
 file.close()
 
 if data_object != None:
     print(data_object["startDate"])
+    feederList = data_object["feeder"]
+    for feeder in feederList :
+        print(feeder["day"])
+        print(feeder["pwmSpeed"])
 
  
