4c745dd6e5556208b9805e92d2590d4ddb7ab8a0
Thong LT <letrthong@gmail.com>
2024-07-07 19:09:39 +0700

    Update parser.py

--- nakehead/parser.py
+++ nakehead/parser.py
@@ -19,29 +19,31 @@ def getCurrentPWM(startDay, currnetNumberOfFish):
     next_day = int( second_next_day/(3600*24)) + 1
     print("next_day=" + str(next_day))
  
-    # file = open("nakehead_feeder.json")
-    # data_object = json.load(file)
-    # file.close()
-    # # "startDate": "02/06/2024",
-    # print(startDay)
-    # if data_object != None:
+    file = open("nakehead_feeder.json")
+    data_object = json.load(file)
+    file.close()
+    # "startDate": "02/06/2024",
+    print(startDay)
+    if data_object != None:
         
-    #     feederList = data_object["feeder"]
-    #     for feeder in feederList :
-    #         print("")
-    #         print("Day=" + str( feeder["day"]))
-    #         print(feeder["pwmSpeed"])
-    #         timerList = feeder["timers"]
-    #         for timer in timerList: 
-    #             print("\n" + timer["startTimer"])
-    #             print(timer["endTimer"])
-    #             print(timer["enable"])
+        feederList = data_object["feeder"]
+        for feeder in feederList :
+            day =  feeder["day"]
+            if day == next_day
+                print("")
+                print("Day=" + str( day))
+                print(feeder["pwmSpeed"])
+                timerList = feeder["timers"]
+                for timer in timerList: 
+                    print("\n" + timer["startTimer"])
+                    print(timer["endTimer"])
+                    print(timer["enable"])
 
 
 startDate="01/06/2024"
 currnetNumberOfFish = 2000
 getCurrentPWM(startDate, currnetNumberOfFish)
  
-startDate="07/07/2024"
-currnetNumberOfFish = 2000
-getCurrentPWM(startDate, currnetNumberOfFish)
+# startDate="07/07/2024"
+# currnetNumberOfFish = 2000
+# getCurrentPWM(startDate, currnetNumberOfFish)
