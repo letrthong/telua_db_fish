0c15947158383ed43121e40118b411c82f9e9e95
Thong LT <letrthong@gmail.com>
2024-07-07 19:04:39 +0700

    Update parser.py

--- nakehead/parser.py
+++ nakehead/parser.py
@@ -9,34 +9,33 @@ def getCurrentPWM(startDay, currnetNumberOfFish):
     print("epoch_time=" + str(epoch_time )  )
     
     # "startDate": "02/06/2024",
-    str_date = "07/07/2024"
-    datetime_object = datetime.datetime.strptime(str_date, "%m/%d/%Y") 
+    datetime_object = datetime.datetime.strptime(startDay, "%m/%d/%Y") 
     print(datetime_object) 
     total_seconds = datetime_object.timestamp()
     print("timestamp=" + str( total_seconds ) )
 
-    next_day = 1 
-    next_epoch_time = epoch_time + ( 3600*next_day)
+    second_next_day = epoch_time  - total_seconds
 
-
+    next_day = int( second_next_day/3600)
+    print("next_day=" + str(next_day))
  
-    file = open("nakehead_feeder.json")
-    data_object = json.load(file)
-    file.close()
-    # "startDate": "02/06/2024",
-    print(startDay)
-    if data_object != None:
+    # file = open("nakehead_feeder.json")
+    # data_object = json.load(file)
+    # file.close()
+    # # "startDate": "02/06/2024",
+    # print(startDay)
+    # if data_object != None:
         
-        feederList = data_object["feeder"]
-        for feeder in feederList :
-            print("")
-            print("Day=" + str( feeder["day"]))
-            print(feeder["pwmSpeed"])
-            timerList = feeder["timers"]
-            for timer in timerList: 
-                print("\n" + timer["startTimer"])
-                print(timer["endTimer"])
-                print(timer["enable"])
+    #     feederList = data_object["feeder"]
+    #     for feeder in feederList :
+    #         print("")
+    #         print("Day=" + str( feeder["day"]))
+    #         print(feeder["pwmSpeed"])
+    #         timerList = feeder["timers"]
+    #         for timer in timerList: 
+    #             print("\n" + timer["startTimer"])
+    #             print(timer["endTimer"])
+    #             print(timer["enable"])
 
 
 startDate="01/06/2024"
