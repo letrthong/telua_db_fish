6ced844d4941080ef5fc49d10f026aa7945dd91e
Thong LT <letrthong@gmail.com>
2024-07-07 17:54:34 +0700

    Update parser.py

--- nakehead/parser.py
+++ nakehead/parser.py
@@ -2,20 +2,24 @@ import json
 import logging
 
  
+def getCurrentPWM():
+    file = open("nakehead_feeder.json")
+    data_object = json.load(file)
+    file.close()
 
-file = open("nakehead_feeder.json")
-data_object = json.load(file)
-file.close()
+    if data_object != None:
+        print(data_object["startDate"])
+        feederList = data_object["feeder"]
+        for feeder in feederList :
+            print(feeder["day"])
+            print(feeder["pwmSpeed"])
+            timerList = feeder["timers"]
+            for timer in timerList: 
+                print(timer["startTimer"])
+                print(timer["endTimer"])
+                print(timer["enable"])
 
-if data_object != None:
-    print(data_object["startDate"])
-    feederList = data_object["feeder"]
-    for feeder in feederList :
-        print(feeder["day"])
-        print(feeder["pwmSpeed"])
-        timerList = feeder["timers"]
-        for timer in timerList: 
-            print(timer["startTimer"])
-            print(timer["endTimer"])
 
+
+getCurrentPWM()
  
